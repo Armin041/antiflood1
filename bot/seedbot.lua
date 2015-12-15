a824028dc029bb31736ff3ee3dce35116d897300
Armin041 <armin.shoja138079@gmail.com>
2015-12-15 14:26:13 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,7 +227,7 @@ function create_config( )
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+150127969
 
 Admins
 @iwals [Founder]
